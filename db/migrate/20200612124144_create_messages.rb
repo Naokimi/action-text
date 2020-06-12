f6babd3b44ea02594227c5ed72603971f74b3e8d
Paulo DAlberti <phdalberti@gmail.com>
2020-06-12 22:23:21 +0900

    dropped redundant column

--- db/migrate/20200612124144_create_messages.rb
+++ db/migrate/20200612124144_create_messages.rb
@@ -2,7 +2,6 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.string :title
-      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2020_06_12_124144) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
-    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
